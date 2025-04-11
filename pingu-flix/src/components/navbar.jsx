226a85d35d33f8fd60bdfbe88ce4c901c3b909b4
doneemoos <doneemooscristian@gmail.com>
2025-04-11 16:13:28 +0300

    NavBar

--- pingu-flix/src/components/navbar.jsx
+++ pingu-flix/src/components/navbar.jsx
@@ -3,27 +3,60 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-4 py-3 bg-gray-800">
-      <div className="text-white text-2xl font-bold">
-        <Link to="/">PinguFlix</Link>
-      </div>
-      <ul className="flex gap-6">
-        <li>
-          <Link className="text-white hover:underline" to="/Movies">
-            Movies
+    <nav className="bg-[oklch(10.1%_0.149_302.717)] w-full py-3 px-1  ">
+      <div className="flex flex-wrap items-center w-full">
+        
+        {/* COL 1: Logo */}
+        {/* 
+          w-full => pe ecrane mici ocupă tot rândul (100%)
+          sm:w-1/3 => de la 640px în sus ocupă 1/3
+          flex justify-center => conținutul este centrat pe orizontală
+          mb-2 => spațiu jos la ecrane mici, sm:mb-0 => scoate spațiul la ecrane mari
+        */}
+        <div className="w-full sm:w-1/3 flex justify-center mb-2 sm:mb-0">
+          <Link to="/" className="text-white text-2xl font-bold">
+            PinguFlix
           </Link>
-        </li>
-        <li>
-          <Link className="text-white hover:underline" to="/Auth">
+        </div>
+
+        {/* COL 2: Meniu principal (Movies, TvShows, Top100) */}
+        {/* 
+          Același pattern: 
+          - 100% lățime pe ecrane mici (și devine un rând separat)
+          - 1/3 la ecrane mari
+          - conținutul centrat pe orizontală
+        */}
+        <div className="w-full sm:w-1/3 flex justify-center mb-2 sm:mb-0">
+          <ul className="flex gap-10">
+            <li>
+              <Link className="text-white hover:underline" to="/Movies">
+                Movies
+              </Link>
+            </li>
+            <li>
+              <Link className="text-white hover:underline" to="/Movies">
+                TvShows
+              </Link>
+            </li>
+            <li>
+              <Link className="text-white hover:underline" to="/Movies">
+                Top100
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* COL 3: Butoane (Log In, Sign Up) */}
+        <div className="w-full sm:w-1/3 flex justify-center">
+          <Link className="text-white hover:underline mr-4" to="/Auth">
             Log In
           </Link>
-        </li>
-        <li>
           <Link className="text-white hover:underline" to="/Auth">
-            Sing Up
+            Sign Up
           </Link>
-        </li>
-      </ul>
+        </div>
+
+      </div>
     </nav>
   );
 };
